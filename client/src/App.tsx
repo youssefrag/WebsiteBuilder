fe4c4d0dfa58a118a4c851d2c89c8a12dd334e7d
Youssef Ragab <ragab.youssef95@gmail.com>
2023-08-03 14:32:16 -0400

    basic navbar and routing

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+import Navigation from "./routes/navigation/navigation.component";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ client/src/routes/navigation/navigation.component.tsx
@@ -0,0 +1,33 @@
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Menu,
+} from "@mui/material";
+
+const Navigation = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          ></IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navigation;
