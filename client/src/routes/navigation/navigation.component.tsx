9b830fc4daeb0bdd40013ea69858064e73f5a74f
Youssef Ragab <ragab.youssef95@gmail.com>
2023-08-03 15:09:54 -0400

    navbar padding fix

--- client/src/routes/navigation/navigation.component.tsx
+++ client/src/routes/navigation/navigation.component.tsx
@@ -11,14 +11,14 @@ import {
 const Navigation = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ margin: 0 }}>
         <Toolbar>
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
+            // sx={{ mr: 2 }}
           ></IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             News
