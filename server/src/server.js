eec2ee2dd1c25ae7ff7301126c9f2b781f6ca6c6
Youssef Ragab <ragab.youssef95@gmail.com>
2023-08-04 19:15:37 -0400

    move mongo file into services

---
+++ server/src/server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+require("dotenv").config();
+
+const app = require("./app");
+const { mongoConnect } = require("./services/mongo");
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+const startServer = async () => {
+  await mongoConnect();
+
+  app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+  });
+};
+
+startServer();
