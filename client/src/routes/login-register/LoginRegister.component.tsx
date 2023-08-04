af425add2f7ea5f88033f0255e24bd072d3661a3
Youssef Ragab <ragab.youssef95@gmail.com>
2023-08-04 14:20:37 -0400

    fix button style bug

--- client/src/routes/login-register/LoginRegister.component.tsx
+++ client/src/routes/login-register/LoginRegister.component.tsx
@@ -1,7 +1,42 @@
-import { PageContainer } from "./LoginRegister.styles";
+import {
+  LoginRegisterContainer,
+  PageContainer,
+  StyledButton,
+  StyledTextfield,
+  StyledTitle,
+  SwitchText,
+  TextfieldContainer,
+} from "./LoginRegister.styles";
 
 const LoginRegister = () => {
-  return <PageContainer>LoginRegister</PageContainer>;
+  return (
+    <PageContainer>
+      <LoginRegisterContainer>
+        <StyledTitle>Register!</StyledTitle>
+        <TextfieldContainer>
+          <StyledTextfield
+            placeholder="Enter Name"
+            name="name"
+          ></StyledTextfield>
+          <StyledTextfield
+            placeholder="Enter Email Address"
+            name="email"
+          ></StyledTextfield>
+          <StyledTextfield
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+          ></StyledTextfield>
+          <StyledTextfield
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+          ></StyledTextfield>
+          <StyledButton>Register</StyledButton>
+        </TextfieldContainer>
+      </LoginRegisterContainer>
+    </PageContainer>
+  );
 };
 
 export default LoginRegister;
--- client/src/routes/login-register/LoginRegister.styles.tsx
+++ client/src/routes/login-register/LoginRegister.styles.tsx
@@ -1,13 +1,68 @@
 import { styled } from "@mui/material/styles";
 
-import { Box, Typography } from "@mui/material";
-
-export const LoginRegisterContainer = styled(Box)(() => ({}));
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 export const PageContainer = styled(Box)(() => ({
-  backgroundColor: "#fdf2e9",
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
   height: "84vh",
+}));
+
+export const LoginRegisterContainer = styled(Box)(() => ({
+  backgroundColor: "#eb984e",
+  width: "80%",
+  height: "80%",
+  borderRadius: "11px",
+  boxShadow: "0 2.4rem 4.8rem rgba(0, 0, 0, 0.15)",
+  padding: "3rem 5rem",
+}));
+
+export const StyledTitle = styled(Typography)(() => ({
+  fontFamily: "Rubik",
+  fontSize: "3rem",
+  fontWeight: "700",
+  color: "#45260a",
+}));
+
+export const Styledtitle = styled(Typography)(() => ({
+  fontFamily: "Rubik",
+  fontSize: "3rem",
+  fontWeight: "700",
+  color: "#45260a",
+}));
+
+export const SwitchText = styled(Typography)(() => ({
+  fontFamily: "Rubik",
+  fontSize: "1rem",
+  fontWeight: "700",
+  color: "#45260a",
+}));
+
+export const TextfieldContainer = styled(Box)(() => ({
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr",
+  marginTop: "2rem",
+  rowGap: "2rem",
+}));
+
+export const StyledTextfield = styled(TextField)(() => ({
+  width: "80%",
+  "& .MuiOutlinedInput-root": {
+    background: "#fff",
+  },
+  "& .MuiInputBase-input": {
+    fontSize: "1.2rem",
+    fontWeight: 700,
+  },
+}));
+
+export const StyledButton = styled(Button)(() => ({
+  color: "#fff",
+  fontFamily: "Rubik",
+  fontSize: "1.3rem",
+  width: "80%",
+  height: "3rem",
+  backgroundColor: "#45260a",
+  textTransform: "capitalize",
 }));
