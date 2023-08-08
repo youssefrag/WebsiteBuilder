6a1351a82d9d48ebc8bd0b08c311851c89ef3ba7
Youssef Ragab <ragab.youssef95@gmail.com>
2023-08-08 12:31:46 -0400

    basic store setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
 
+import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./App";
 
@@ -11,8 +13,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    {/* <Provider store={}> */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    {/* </Provider> */}
   </React.StrictMode>
 );
---
+++ client/src/store/user/userSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginUser: (state) => {
+      state.user = state.payload;
+    },
+    logoutUser: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { loginUser, logoutUser } = userSlice.actions;
+
+export default userSlice.reducer;
