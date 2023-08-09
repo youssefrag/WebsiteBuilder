9598d6755bc7ec3c92df82dd7474715d6e58bb58
Youssef Ragab <ragab.youssef95@gmail.com>
2023-08-09 11:15:11 -0400

    style new site entry

--- client/src/routes/websites/websites.component.tsx
+++ client/src/routes/websites/websites.component.tsx
@@ -1,5 +1,19 @@
+import {
+  CreateNew,
+  PageContainer,
+  PageTitle,
+  SitesContainer,
+} from "./websites.styles";
+
 const Websites = () => {
-  return <div>Websites</div>;
+  return (
+    <PageContainer>
+      <PageTitle>My Websites</PageTitle>
+      <SitesContainer>
+        <CreateNew>Create new site!</CreateNew>
+      </SitesContainer>
+    </PageContainer>
+  );
 };
 
 export default Websites;
--- client/src/routes/websites/websites.styles.tsx
+++ client/src/routes/websites/websites.styles.tsx
@@ -1,1 +1,36 @@
-export {};
+import { styled } from "@mui/material/styles";
+
+import { Button, Box, Typography } from "@mui/material";
+
+export const PageContainer = styled(Box)(() => ({
+  margin: "4rem 6rem",
+}));
+
+export const PageTitle = styled(Typography)(() => ({
+  fontFamily: "Rubik",
+  fontSize: "2rem",
+  fontWeight: "700",
+  letterSpacing: "-0.5px",
+  lineHeight: "1.2",
+  marginBottom: "3.2rem",
+  color: "#333",
+}));
+
+export const SitesContainer = styled(Box)(() => ({
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr 1fr 1fr",
+}));
+
+export const CreateNew = styled(Button)(() => ({
+  height: "16rem",
+  width: "16rem",
+  backgroundColor: "#e67e22",
+  borderRadius: "50%",
+  fontFamily: "Rubik",
+  color: "#fff",
+  fontSize: "1.5rem",
+  "&:hover": {
+    backgroundColor: "#fdf2e9",
+    color: "#e67e22",
+  },
+}));
