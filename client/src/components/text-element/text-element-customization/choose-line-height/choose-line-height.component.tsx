9d29807d6bce7a54c91e6c4a25365600ddf23ad9
Youssef Ragab <ragab.youssef95@gmail.com>
2023-08-11 13:30:39 -0400

    finish edit text frontend

--- client/src/components/text-element/text-element-customization/choose-line-height/choose-line-height.component.tsx
+++ client/src/components/text-element/text-element-customization/choose-line-height/choose-line-height.component.tsx
@@ -1,27 +1,27 @@
-import { Box, InputLabel, MenuItem, Select } from "@mui/material";
-import { SelectChangeEvent } from "@mui/material/Select";
-
-import { StyledFormControl } from "./choose-line-height.styles";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 
 const ChooseLineHeight = () => {
   return (
-    <Box sx={{ minWidth: 120 }}>
-      <StyledFormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Line Height</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          //   value={canvas?.fontType}
-          label="Age"
-          //   onChange={handleChange}
-        >
-          <MenuItem value={"1"}>1</MenuItem>
-          <MenuItem value={"1.2"}>1.2</MenuItem>
-          <MenuItem value={"1.4"}>1.4</MenuItem>
-          <MenuItem value={"1.6"}>1.6</MenuItem>
-        </Select>
-      </StyledFormControl>
-    </Box>
+    <FormControl sx={{ "& .MuiFormControl-root": { width: "40%" } }}>
+      <FormLabel id="demo-controlled-radio-buttons-group">
+        Line Height
+      </FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-controlled-radio-buttons-group"
+        name="controlled-radio-buttons-group"
+        // value={canvas?.fontSize}
+        // onChange={handleChange}
+      >
+        <FormControlLabel value="1.2" control={<Radio />} label="1.2" />
+        <FormControlLabel value="1.4" control={<Radio />} label="1.4" />
+        <FormControlLabel value="1.6" control={<Radio />} label="1.6" />
+        <FormControlLabel value="1.8" control={<Radio />} label="1.8" />
+      </RadioGroup>
+    </FormControl>
   );
 };
 
