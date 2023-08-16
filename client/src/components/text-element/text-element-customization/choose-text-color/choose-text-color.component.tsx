5ec44944110a67cbac70f776776b7cadb80b0c82
Youssef Ragab <ragab.youssef95@gmail.com>
2023-08-16 17:26:38 -0400

    finish customization text ui

--- client/src/components/text-element/text-element-customization/choose-text-color/choose-text-color.component.tsx
+++ client/src/components/text-element/text-element-customization/choose-text-color/choose-text-color.component.tsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { editCanvas, selectCanvas } from "../../../../store/canvas/canvasSlice";
+
 import { HexColorPicker } from "react-colorful";
 
 const ChooseTextColor = () => {
   const [color, setColor] = useState("#e67e22");
+  const canvas = useSelector(selectCanvas);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (canvas !== null) {
+      dispatch(editCanvas({ ...canvas, color: color }));
+    }
+  }, [color]);
+
   return <HexColorPicker color={color} onChange={setColor} />;
 };
 
