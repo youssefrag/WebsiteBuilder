5340906bf753e17e86a432cb9fdc5cc5c8314dae
Youssef Ragab <ragab.youssef95@gmail.com>
2023-08-18 18:16:07 -0400

    save modal ui complete

--- client/src/components/save-modal/save-modal.component.tsx
+++ client/src/components/save-modal/save-modal.component.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { Box, Button, Typography, Modal } from "@mui/material";
+import { useSelector } from "react-redux";
+
+import { selectUser } from "../../store/user/userSlice";
+
+import { Box, Modal } from "@mui/material";
 
 import {
   SaveButton,
@@ -10,9 +15,33 @@ import {
 } from "./save-modal.styles";
 
 const SaveModal = () => {
+  let navigate = useNavigate();
+
+  const currentUser = useSelector(selectUser);
+
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const [websiteName, setWebsiteName] = useState<string>("");
+
+  const handleWebsiteNameChange = (e: React.FormEvent) => {
+    const element = e.currentTarget as HTMLInputElement;
+    const value = element.value;
+    const name = element.name;
+    setWebsiteName(value);
+  };
+
+  const [authorEmail, setAuthorEmail] = useState<string>("");
+
+  const handleAuthorEmailChange = (e: React.FormEvent) => {
+    const element = e.currentTarget as HTMLInputElement;
+    const value = element.value;
+    setAuthorEmail(value);
+  };
+
+  const handleSave = async () => {};
+
   return (
     <div>
       <SaveButton onClick={handleOpen}>Save Website</SaveButton>
@@ -23,8 +52,20 @@ const SaveModal = () => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <StyledTextfield />
-          <StyledTextfield />
+          <StyledTextfield
+            value={websiteName}
+            placeholder="Enter website name"
+            name="email"
+            onChange={handleWebsiteNameChange}
+          />
+          {!currentUser && (
+            <StyledTextfield
+              value={authorEmail}
+              placeholder="Enter user email"
+              name="email"
+              onChange={handleAuthorEmailChange}
+            />
+          )}
           <StyledButton>Save</StyledButton>
         </Box>
       </Modal>
