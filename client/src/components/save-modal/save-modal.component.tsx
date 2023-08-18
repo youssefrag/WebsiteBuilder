83ef64a8c2b8cd801211bb04084b6d7e4adc5c12
Youssef Ragab <ragab.youssef95@gmail.com>
2023-08-18 17:54:09 -0400

    style save modal

--- client/src/components/save-modal/save-modal.component.tsx
+++ client/src/components/save-modal/save-modal.component.tsx
@@ -2,19 +2,12 @@ import { useState } from "react";
 
 import { Box, Button, Typography, Modal } from "@mui/material";
 
-import { SaveButton } from "./save-modal.styles";
-
-const style = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
+import {
+  SaveButton,
+  StyledTextfield,
+  StyledButton,
+  style,
+} from "./save-modal.styles";
 
 const SaveModal = () => {
   const [open, setOpen] = useState(false);
@@ -30,12 +23,9 @@ const SaveModal = () => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <StyledTextfield />
+          <StyledTextfield />
+          <StyledButton>Save</StyledButton>
         </Box>
       </Modal>
     </div>
--- client/src/components/save-modal/save-modal.styles.tsx
+++ client/src/components/save-modal/save-modal.styles.tsx
@@ -1,5 +1,5 @@
 import { styled } from "@mui/material/styles";
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 
 export const SaveButton = styled(Button)(() => ({
   fontFamily: "Rubik",
@@ -12,3 +12,47 @@ export const SaveButton = styled(Button)(() => ({
     backgroundColor: "#fff",
   },
 }));
+
+export const StyledTextfield = styled(TextField)(() => ({
+  width: "100%",
+  "& .MuiOutlinedInput-root": {
+    background: "#fff",
+  },
+  "& .MuiInputBase-input": {
+    color: "#666",
+    fontFamily: "Rubik",
+    fontSize: "1.2rem",
+    fontWeight: 700,
+  },
+}));
+
+export const StyledButton = styled(Button)(() => ({
+  color: "#fff",
+  fontFamily: "Rubik",
+  fontSize: "1.3rem",
+  width: "100%",
+  height: "3.2rem",
+  backgroundColor: "#45260a",
+  textTransform: "capitalize",
+  borderRadius: "9px",
+  "&:hover": {
+    backgroundColor: "#fff",
+    color: "#45260a",
+  },
+}));
+
+export const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "#eea564",
+  boxShadow: 10,
+  borderRadius: "20px",
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
+  alignItems: "center",
+};
