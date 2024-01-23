6a80f3251bf9d9885b9ba544d3741e6bfd978bd3
Youssef Ragab <ragab.youssef95@gmail.com>
2024-01-23 15:24:58 -0500

    only post requests working

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.use(express.json());
 
 // app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.static(path.join(__dirname, "../client/build")));
+console.log(path.join(__dirname, "../client/build"));
 if (process.env.NODE_ENV === "production") {
   //serve static content
   //npm run build
