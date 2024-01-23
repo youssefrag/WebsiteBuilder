f3557ecbfa8ed16549004e2d690e04800beff18b
Youssef Ragab <ragab.youssef95@gmail.com>
2024-01-23 14:17:41 -0500

    only post requests working

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import { Provider } from "react-redux";
 
@@ -15,10 +15,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
+    <Router>
+      <Provider store={store}>
         <App />
-      </BrowserRouter>
-    </Provider>
+      </Provider>
+    </Router>
   </React.StrictMode>
 );
